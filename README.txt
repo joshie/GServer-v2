a306d1fe6f3dc780b431e09cdcbca85dbaea6865
Marlon <Marlon@7aab1f78-3e5b-11de-917f-4137a9cc17b9>
2008-10-30 17:04:01 +0000

    * seipheroth branch: Files added.

--- README.txt
+++
@@ -1,1 +0,0 @@
-Placeholder for Seipheroth's server.
---
+++ server/src/TPlayerLogin.cpp
@@ -0,0 +1,237 @@
+#include <vector>
+#include <math.h>
+#include "ICommon.h"
+#include "TPlayer.h"
+#include "TAccount.h"
+#include "CSocket.h"
+#include "TServerList.h"
+
+#define serverlog	server->getServerLog()
+#define rclog		server->getRCLog()
+extern bool __sendLogin[propscount];
+extern bool __getLogin[propscount];
+extern bool __getLoginRC[propscount];
+
+/*
+	TPlayer: Manage Account
+*/
+bool TPlayer::sendLogin()
+{
+	// Load Player-Account
+	// TODO: We actually need to check if it fails because accounts can be banned.
+	loadAccount(accountName); // We don't need to check if this fails.. because the defaults have already been loaded :)
+
+	// Server Signature
+	// 0x49 (73) is used to tell the client that more than eight
+	// players will be playing.
+	sendPacket(CString() >> (char)PLO_SIGNATURE >> (char)73);
+
+	// If we have an NPC Server, send this to prevent clients from sending
+	// npc props it modifies.
+	sendPacket(CString() >> (char)PLO_HASNPCSERVER);
+
+	// Check if the account is already in use.
+	std::vector<TPlayer*>* playerList = server->getPlayerList();
+	for (std::vector<TPlayer*>::iterator i = playerList->begin(); i != playerList->end(); ++i)
+	{
+		TPlayer* player = *i;
+		CString oacc = player->getProp(PLPROP_ACCOUNTNAME).subString(1);
+		unsigned short oid = player->getProp(PLPROP_ID).readGUShort();
+		if (oacc == accountName && player->getType() == type && oid != id)
+		{
+			if ((int)difftime(time(0), player->getLastData()) > 30)
+			{
+				player->sendPacket(CString() >> (char)PLO_DISCMESSAGE << "Someone else has logged into your account.");
+				player->disconnect();
+			}
+			else
+			{
+				sendPacket(CString() >> (char)PLO_DISCMESSAGE << "Account is already in use.");
+				return false;
+			}
+		}
+	}
+
+	// Player's load different than RCs.
+	bool succeeded = false;
+	if (type == CLIENTTYPE_CLIENT) succeeded = sendLoginClient();
+	else if (type == CLIENTTYPE_RC) succeeded = sendLoginRC();
+	if (succeeded == false) return false;
+
+	// Exchange props with everybody on the server.
+	for (std::vector<TPlayer*>::iterator i = playerList->begin(); i != playerList->end(); ++i)
+	{
+		TPlayer* player = (TPlayer*)*i;
+		if (player == this) continue;
+
+		// Get the other player's props.
+		if (player->getType() == CLIENTTYPE_CLIENT)
+		{
+			// Send my props to the player.
+			player->sendPacket(this->getProps(__getLogin, sizeof(__getLogin)/sizeof(bool)));
+
+			// Get their props.
+			this->sendPacket(player->getProps(__getLogin, sizeof(__getLogin)/sizeof(bool)));
+		}
+		else if (player->getType() == CLIENTTYPE_RC)
+		{
+			// Send the players props to the RC's.
+			CString sPlayerProps;
+			sPlayerProps >> (char)PLO_ADDPLAYER        >> (short)id;
+			sPlayerProps >> (char)accountName.length() << accountName;
+			sPlayerProps >> (char)PLPROP_NICKNAME      >> (char)nickName.length() << nickName;
+			sPlayerProps >> (char)PLPROP_PSTATUSMSG    >> (char)statusMsg;
+			sPlayerProps >> (char)PLPROP_COMMUNITYNAME >> (char)communityName.length() << communityName;
+			player->sendPacket(sPlayerProps);
+
+			// Send the other players props.
+			CString sOtherProps;
+			sOtherProps >> (char)PLO_ADDPLAYER        >> (short)player->id;
+			sOtherProps >> (char)player->accountName.length() << player->accountName;
+			sOtherProps >> (char)PLPROP_NICKNAME      >> (char)player->nickName.length() << player->nickName;
+			sOtherProps >> (char)PLPROP_PSTATUSMSG    >> (char)player->statusMsg;
+			sOtherProps >> (char)PLPROP_COMMUNITYNAME >> (char)player->communityName.length() << player->communityName;
+			sendPacket(sOtherProps);
+		}
+	}
+
+	// Tell the serverlist that the player connected.
+	server->getServerList()->addPlayer(this);
+
+	sendCompress();
+	return true;
+}
+
+bool TPlayer::sendLoginClient()
+{
+	CSettings* settings = server->getSettings();
+
+	// Send the player his login props.
+	sendProps(__sendLogin, sizeof(__sendLogin) / sizeof(bool));
+
+	// Workaround for 2.2+ clients.  They don't request the map file when used with setmap.
+	// So, just send them all the maps loaded into the server.
+	if (PLE_POST22)
+	{
+		for (std::vector<TMap*>::iterator i = server->getMapList()->begin(); i != server->getMapList()->end(); ++i)
+		{
+			TMap* map = *i;
+			if (map->getType() == MAPTYPE_BIGMAP)
+				msgPLI_WANTFILE(CString() << map->getMapName());
+		}
+	}
+
+	// Send the level to the player.
+	// warp will call sendCompress() for us.
+	if (warp(levelName, x, y) == false)
+	{
+		sendPacket(CString() >> (char)PLO_DISCMESSAGE << "No level available.");
+		serverlog.out(CString() << "Cannot find level for " << accountName << "\n");
+		return false;
+	}
+
+	// Recalculate player spar deviation.
+	{
+		// c = sqrt( (350*350 - 50*50) / t )
+		// where t = 60 for number of rating periods for deviation to go from 50 to 350
+		const float c = 44.721f;
+		float t = (float)(time(0) - lastSparTime)/86400.0f; // Convert seconds to days: 60/60/24
+
+		// Find the new deviation.
+		float deviate = MIN( sqrt((oldDeviation*oldDeviation) + (c*c) * t), 350.0f );
+
+		// Save the old rating and set the new one.
+		deviation = deviate;
+	}
+
+	// Send out what guilds should be placed in the Staff section of the playerlist.
+	std::vector<CString> guilds = settings->getStr("staffguilds").tokenize(",");
+	CString guildPacket = CString() >> (char)PLO_STAFFGUILDS;
+	for (std::vector<CString>::iterator i = guilds.begin(); i != guilds.end(); ++i)
+		guildPacket << "\"" << ((CString)(*i)).trim() << "\",";
+	sendPacket(guildPacket);
+
+	// Send out the server's available status list options.
+	std::vector<CString> plicons = settings->getStr("playerlisticons").tokenize(",");
+	CString pliconPacket = CString() >> (char)PLO_STATUSLIST;
+	for (std::vector<CString>::iterator i = plicons.begin(); i != plicons.end(); ++i)
+		pliconPacket << "\"" << ((CString)(*i)).trim() << "\",";
+	sendPacket(pliconPacket);
+
+	// Send out RPG Window greeting.
+	sendPacket(CString() >> (char)PLO_RPGWINDOW << "\"Welcome to " << settings->getStr("name") << ".\",\"Graal Reborn GServer programmed by Joey and Nalin.\"" );
+
+	// Send the start message to the player.
+	sendPacket(CString() >> (char)PLO_STARTMESSAGE << *(server->getServerMessage()));
+
+	// Delete the bomb.  It gets automagically added by the client for
+	// God knows which reason.  Bomb must be capitalized.
+	sendPacket(CString() >> (char)PLO_NPCWEAPONDEL << "Bomb");
+
+	// Send the player's weapons.
+	for (std::vector<CString>::iterator i = weaponList.begin(); i != weaponList.end(); ++i)
+	{
+		TWeapon* weapon = server->getWeapon(*i);
+		if (weapon == 0)
+		{
+			// Let's check to see if it is a default weapon.  If so, we can add it to the server now.
+			int wId = TLevelItem::getItemId(*i);
+			if (wId != -1)
+			{
+				CString defWeapPacket = CString() >> (char)PLI_WEAPONADD >> (char)0 >> (char)wId;
+				defWeapPacket.readGChar();
+				msgPLI_WEAPONADD(defWeapPacket);
+				continue;
+			}
+		}
+		sendPacket(weapon->getWeaponPacket());
+	}
+
+	// Send the player's flags.
+	for (std::vector<CString>::iterator i = flagList.begin(); i != flagList.end(); ++i)
+		sendPacket(CString() >> (char)PLO_FLAGSET << *i);
+
+	// Send the server's flags to the player.
+	std::vector<CString>* serverFlags = server->getServerFlags();
+	for (std::vector<CString>::iterator i = serverFlags->begin(); i != serverFlags->end(); ++i)
+		sendPacket(CString() >> (char)PLO_FLAGSET << *i);
+
+	return true;
+}
+
+bool TPlayer::sendLoginRC()
+{
+	// If no nickname was specified, set the nickname to the account name.
+	if (nickName.length() == 0)
+		nickName = accountName;
+
+	// Set the head to the server's set staff head.
+	headImg = server->getSettings()->getStr("staffhead", "head25.png");
+
+	// Send the RC join message to the RC.
+	// TODO: Custom RC join message (rcmessage.txt)
+	sendPacket(CString() >> (char)PLO_RCMESSAGE << "Welcome to the GServer for " << accountName << ".");
+
+	// Send the details about the NPC-Server.
+	// second 0 = ID.
+	CString npcServer;
+	npcServer >> (char)PLO_ADDPLAYER        >> (short)2;
+	npcServer >> (char)11 << "(npcserver)";
+	npcServer >> (char)PLPROP_NICKNAME      >> (char)19 << "NPC-Server (Server)";
+	npcServer >> (char)PLPROP_PSTATUSMSG    >> (char)0;
+	npcServer >> (char)PLPROP_COMMUNITYNAME >> (char)11 << "(npcserver)";
+	sendPacket(npcServer);
+
+	// Send the RC the address and port of the NPC-Server.
+	sendPacket(CString() >> (char)PLO_NPCSERVERADDR >> (char)0 >> (char)2 << server->getNPCServerAddr());
+
+	CString t = (CString() >> (char)PLO_NPCSERVERADDR >> (char)0 >> (char)2 << server->getNPCServerAddr());
+	FILE* f = fopen("dump.bin", "wb");
+	fwrite(t.text(), t.length(), 1, f);
+	fclose(f);
+	printf("Dumped packet to file...\n");
+
+	server->sendPacketTo(CLIENTTYPE_RC, CString() >> (char)PLO_RCMESSAGE << "New RC: " << nickName << " (" << accountName << ")");
+	
+	return true;
+}
